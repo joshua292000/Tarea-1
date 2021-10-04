401b362059a52602f4b01a4111cfd50b240410cc
joshua292000 <joshua292000@gmail.com>
2021-10-04 16:45:25 -0600

    cambio

--- Ejemplo del patron Flyweight/src/main/java/com/example/bosque/Demo.java
+++ Ejemplo del patron Flyweight/src/main/java/com/example/bosque/Demo.java
@@ -18,14 +18,14 @@ public class Demo {
         forest.setSize(CANVAS_SIZE, CANVAS_SIZE);
         forest.setVisible(true);
 
-        System.out.println(TREES_TO_DRAW + " trees drawn");
+        System.out.println(TREES_TO_DRAW + " arboles dibujados");
         System.out.println("---------------------");
-        System.out.println("Memory usage:");
-        System.out.println("Tree size (8 bytes) * " + TREES_TO_DRAW);
-        System.out.println("+ TreeTypes size (~30 bytes) * " + TREE_TYPES + "");
+        System.out.println("Uso de la memoria:");
+        System.out.println("Tamano del arbol (8 bytes) * " + TREES_TO_DRAW);
+        System.out.println("+ Tamano del tipo de arbol (~30 bytes) * " + TREE_TYPES + "");
         System.out.println("---------------------");
         System.out.println("Total: " + ((TREES_TO_DRAW * 8 + TREE_TYPES * 30) / 1024 / 1024) +
-                "MB (instead of " + ((TREES_TO_DRAW * 38) / 1024 / 1024) + "MB)");
+                "MB (memoria que seria usada si no se implementara flyweight " + ((TREES_TO_DRAW * 38) / 1024 / 1024) + "MB)");
     }
 
     private static int random(int min, int max) {
